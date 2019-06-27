9f8ed977ca4d96e3fb3adbbfd65de2e05772e284
kashinkoji <chandan18386@iiitd.ac.in>
2019-06-27 23:48:24 +0530

    basic messging done

--- src/index.js
+++ src/index.js
@@ -1,14 +1,25 @@
 const http = require('http');
 var path = require('path');
 var express = require('express');
+const socketio = require('socket.io');
 
-var app = express();
+const app = express();
 const server = http.createServer(app);
+const io = socketio(server);
 
 const port = process.env.PORT || 3000;
-const pubDirectory = path.join(__dirname,'../public');
+const pubDirectory = path.join(__dirname, '../public');
 
 app.use(express.static(pubDirectory));
+
+let count = 0;
+
+io.on('connection',(socket) => {
+  socket.emit('message','Welcome!')
+  socket.on('sendMessage',(message) => {
+    io.emit('message',message);
+  });
+});
 
 server.listen(port,() => {
   console.log(`Server listening on port ${port}...`);

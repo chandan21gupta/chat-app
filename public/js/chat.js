27ab955a5f0d17a2fa7298b34a944cf1ca210fc0
kashinkoji <chandan18386@iiitd.ac.in>
2019-06-28 13:48:35 +0530

    slight modifications

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,16 +1,29 @@
 const socket = io();
 
+const $messageForm = document.querySelector('#message-form');
+const $messageFormInput = $messageForm.querySelector('input');
+const $messageFormButton = $messageForm.querySelector('button');
+const $sendLocationButton = document.querySelector('#send-location');
+
+
 socket.on('message',(message) => {
   console.log(message);
 });
 
 
-document.querySelector('#message-form').addEventListener('submit', (e) => {
+$messageForm.addEventListener('submit', (e) => {
   e.preventDefault();
+
+  $messageFormButton.setAttribute('disabled','disabled');
 
   const message = document.querySelector('input').value;
 
   socket.emit('sendMessage',message,() => {
+
+    $messageFormButton.removeAttribute('disabled');
+    $messageFormInput.value = '';
+    $messageFormInput.focus();
+
     console.log('The message was delivered');
   });
 
@@ -22,12 +35,15 @@ document.querySelector('#send-location').addEventListener('click', () => {
     return alert('Geolocation is not supported by your browser');
   }
 
+  $sendLocationButton.setAttribute('disabled','disabled');
+
 navigator.geolocation.getCurrentPosition((position) => {
   console.log(position);
   socket.emit('sendlocation', {
     latitude : position.coords.latitude,
     longitude : position.coords.longitude
   },() => {
+    $sendLocationButton.removeAttribute('disabled');
     console.log('Location Shared!');
   });
 });
